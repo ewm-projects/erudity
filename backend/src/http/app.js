61ba6c7ed01f348e900adb69b7e7fbe5d8463c58
rezbee-dev <131298724+rezbee-dev@users.noreply.github.com>
2023-06-18 12:34:38 -0400

    init express factory

---
+++ backend/src/http/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import "express-async-errors";
+
+import { env } from "../common/env.js";
+
+
+const create = () => {
+    const app = express()
+    app.use(express.json())
+    return app
+}
+
+const start = () => {
+    const getUrl = (server) => {
+        const address = server.address()
+        const port = address.port !== 80 ? `:${address.port}` : "";
+        let host = "";
+
+        if (address.family === "IPv4" && address.address !== "0.0.0.0") {
+            host = address.address;
+          } else if (address.family === "IPv6" && address.address !== "::") {
+            host = address.address;
+          } else {
+            host = "localhost";
+          }
+        
+          return `http://${host}${port}/`;
+    }
+
+    return new Promise((resolve, reject) => {
+        const app = create()
+        const server = app.listen(env.PORT);
+
+        server.once("listening", () => resolve(`Server running on ${getUrl(server)}`))
+        server.once("error", (err) => reject(new Error(`Server failed to start: ${err.message}`)));
+    })
+}
+
+const app = {create, start}
+
+export default app
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,17 @@
 import { env } from "./common/env.js";
+import app from "./http/app.js";
 
-console.log('Hello world!');
-console.log("Port", env.PORT)
-console.log("NODE ENV", env.NODE_ENV)
+const main = async () => {
+    console.log('Hello world!');
+    console.log("Port", env.PORT)
+    console.log("NODE ENV", env.NODE_ENV)
+    
+    try {
+        console.log(await app.start())
+    } catch (e) {
+        console.error("Failed to start application")
+        throw e
+    }
+}
+
+main().catch((e) => console.log(e.message))
