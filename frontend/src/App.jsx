7b908fc9f5beee0c03534a4d0696d92d951b055b
rezbee-dev <131298724+rezbee-dev@users.noreply.github.com>
2023-06-18 18:48:40 -0400

    add tailwind & daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,14 @@
-import { useState } from "react";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div className="alert">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+        <span>12 unread messages. Tap to see.</span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
