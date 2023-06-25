ca2e2b08c31c9712074a4630418fa8b8f8ed603a
rezbee-dev <131298724+rezbee-dev@users.noreply.github.com>
2023-06-25 17:32:56 -0400

    Rename PingModel to PingDao

--- backend/src/main/modules/ping/ping.repository.js
+++ backend/src/main/modules/ping/ping.repository.js
@@ -1,11 +1,11 @@
-import { PingModel } from "./ping.model.js";
+import { PingDao } from "./ping.dao.js";
 
-const add = (dto) => PingModel.create(dto);
-const getAll = () => PingModel.find({}).exec();
-const getById = (id) => PingModel.findById(id).exec();
-const removeById = (id) => PingModel.findByIdAndDelete(id).exec();
+const add = (dto) => PingDao.create(dto);
+const getAll = () => PingDao.find({}).exec();
+const getById = (id) => PingDao.findById(id).exec();
+const removeById = (id) => PingDao.findByIdAndDelete(id).exec();
 const updateById = (id, dto) =>
-  PingModel.findByIdAndUpdate(id, dto, { new: true }).exec();
+  PingDao.findByIdAndUpdate(id, dto, { new: true }).exec();
 
 export const PingRepository = {
   add,
