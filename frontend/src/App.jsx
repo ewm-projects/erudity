cc70999e8a48a1aae86f9a714fdfdc799ee9668e
rezbee-dev <131298724+rezbee-dev@users.noreply.github.com>
2023-07-10 12:22:05 -0400

    setup layout & fix navbar mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import LandingPage from "./pages/Landing";
 import AboutPage from "./pages/About";
 import NotFoundPage from "./pages/NotFound";
 import PingPage from "./pages/Ping";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Layout from "./components/Layout";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<LandingPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/ping" element={<PingPage />} />
         <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-      <Footer />
-    </Router>
+      </Route>
+    )
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+
+// import { BrowserRouter as Router, Routes, Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+
+// return (
+//   <Router>
+//     <Layout />
+//     <Routes >
+//       <Route path="/" element={<LandingPage />} />
+//       <Route path="/about" element={<AboutPage />} />
+//       <Route path="/ping" element={<PingPage />} />
+//       <Route path="*" element={<NotFoundPage />} />
+//     </Routes>
+//   </Router>
+// );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -50,6 +50,72 @@ function Navbar() {
         </div>
       </div>
       <div className="navbar-end">
+        <div className="hidden md:flex">
+          <button className="btn btn-ghost">Login</button>
+          <button className="btn btn-ghost">Signup</button>
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Profile</a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost md:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-36"
+          >
+            <li>
+              <a>Profile</a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+
+{
+  /* <div className="navbar-end hidden md:flex">
         <button className="btn btn-ghost">Login</button>
         <button className="btn btn-ghost">Signup</button>
         <div className="dropdown dropdown-end">
@@ -72,10 +138,5 @@ function Navbar() {
               <a>Logout</a>
             </li>
           </ul>
-        </div>
-      </div>
-    </div>
-  );
+        </div> */
 }
-
-export default Navbar;
